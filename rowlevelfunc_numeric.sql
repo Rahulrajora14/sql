b3257c44809bffe03252b7c824452b990a99e7c5
Rahulrajora14 <rahulrajora_23cs328@dtu.ac.in>
2026-07-06 23:47:44 +0530

    Add files via upload

---
+++ rowlevelfunc_numeric.sql
@@ -0,0 +1,133 @@
+-- ABS()
+
+SELECT ABS(-25);
+
+
+-- CEIL()
+
+SELECT CEIL(25.3);
+
+
+-- FLOOR()
+
+SELECT FLOOR(25.9);
+
+
+-- ROUND()
+
+SELECT ROUND(25.567, 2);
+
+
+-- TRUNCATE()
+
+SELECT TRUNCATE(25.5678, 2);
+
+
+-- MOD()
+
+SELECT MOD(17, 5);
+
+
+-- POWER()
+
+SELECT POWER(2, 5);
+
+
+-- SQRT()
+
+SELECT SQRT(144);
+
+
+-- EXP()
+
+SELECT EXP(2);
+
+
+-- LOG()
+
+SELECT LOG(10);
+
+
+-- LOG10()
+
+SELECT LOG10(1000);
+
+
+-- PI()
+
+SELECT PI();
+
+
+-- RAND()
+
+SELECT RAND();
+
+
+-- SIGN()
+
+SELECT SIGN(-45);
+
+
+-- GREATEST()
+
+SELECT GREATEST(25, 48, 10, 67);
+
+
+-- LEAST()
+
+SELECT LEAST(25, 48, 10, 67);
+
+
+-- SIN()
+
+SELECT SIN(PI()/2);
+
+
+-- COS()
+
+SELECT COS(0);
+
+
+-- TAN()
+
+SELECT TAN(PI()/4);
+
+
+-- RADIANS()
+
+SELECT RADIANS(180);
+
+
+-- DEGREES()
+
+SELECT DEGREES(PI());
+
+
+-- AVG()
+
+SELECT AVG(salary)
+FROM Employees;
+
+
+-- SUM()
+
+SELECT SUM(salary)
+FROM Employees;
+
+
+-- MAX()
+
+SELECT MAX(salary)
+FROM Employees;
+
+
+-- MIN()
+
+SELECT MIN(salary)
+FROM Employees;
+
+
+-- COUNT()
+
+SELECT COUNT(*)
+FROM Employees;
